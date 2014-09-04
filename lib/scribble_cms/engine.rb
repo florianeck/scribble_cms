da7cabf5ceeacf05e228f21cfd3c1d500d8678c3
Florian Eck <florian.eck@financial-friends.de>
2014-09-04 18:47:35 +0200

    addedscretet

--- lib/scribble_cms/engine.rb
+++ lib/scribble_cms/engine.rb
@@ -8,10 +8,9 @@ module Scribbler
       # Pictures
       Dragonfly.app(:scribbler).configure do
         plugin :imagemagick
-        datastore :file,
-          root_path:  Rails.root.join('public/system/uploads/scribbler').to_s,
-          store_meta: false
+        datastore :file, root_path:  Rails.root.join('public/system/uploads/scribbler').to_s, store_meta: false
         url_format "/s/static/pictures/:job/:name.:format"
+        secret Digest::SHA1.hexdigest(Rails.root.to_s)
       end
       
       
