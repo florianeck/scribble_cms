945fb569330763ae6cd39b72c49cce48cc7c30d0
Florian Eck <it-support@friends-systems.de>
2015-11-25 23:07:36 +0100

    fixed humanize bug

--- lib/scribble_cms/scribbler_element_setup.rb
+++ lib/scribble_cms/scribbler_element_setup.rb
@@ -1,6 +1,6 @@
 module ScribbleCms
   module ScribblerElementSetup
-    
+
     def self.included(base)
       base.send(:belongs_to, :group, :class_name => "ScribblerGroup", :foreign_key => "group_id", :touch => true)
       base.send(:validates_presence_of, :group_id)
@@ -10,29 +10,29 @@ module ScribbleCms
         ],
         :conditions => "released = 1"
       )
-      
+
     end
-    
-    
+
+
     def check_uniqueness
       items = self.class.where(:name => self.name, :group_id => self.group_id, :released => self.released)
       items.empty? || items.size == 1
-    end 
-    
+    end
+
     def element_type
       if self.is_a?(ScribblerLink)
         return :link
       elsif self.is_a?(ScribblerText) && self.plain
         return :row
-      elsif self.is_a?(ScribblerText) && !self.plain   
+      elsif self.is_a?(ScribblerText) && !self.plain
         return :text
-      elsif self.is_a?(ScribblerImage)  
+      elsif self.is_a?(ScribblerImage)
         return :image
       elsif self.is_a?(ScribblerVar)
-        return :var  
-      end  
+        return :var
+      end
     end
-    
+
     def searchable_content
       case self.element_type
       when :link
@@ -43,17 +43,17 @@ module ScribbleCms
         return content
       when :image
       when :var
-      end        
-    end  
-    
+      end
+    end
+
     def human_name
-      I18n.t(self.name, :scope => "scribbler.element_names.#{self.group.name}", :default => self.name.humanize)
-    end     
-    
-    def hint  
+      I18n.t(self.name, :scope => "scribbler.element_names.#{self.group.name}", :default => self.name.to_s.humanize)
+    end
+
+    def hint
       h = I18n.t(self.name, :scope => "scribbler.element_hints.#{self.group.name}", :default => "")
       h.is_a?(Array) ? h.join("") : h
-    end  
-    
+    end
+
   end
-end  
+end
