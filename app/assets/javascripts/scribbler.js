eb201b1d6912406c5d191bd4bcb7743793597016
Florian Eck <florian.eck@el-digital.de>
2017-06-21 13:53:25 +0200

    formatting

--- app/assets/javascripts/scribbler.js
+++ app/assets/javascripts/scribbler.js
@@ -3,15 +3,15 @@
 
 window.initScribblerTinyMce = function(){
   tinymce.remove("textarea")
- tinyMCE.init({
-   mode: 'textareas',
-   theme: 'modern',
-   plugins: "autolink code colorpicker hr image link lists paste table textcolor",
-   menubar: '',
-   toolbar: "undo redo | styleselect | bold italic | link unlink image | alignleft aligncenter alignright | bullist numlist table | textcolor | code",
-   valid_elements : '*[*]',
-   elementpath: false,
-   statusbar: false
+  tinyMCE.init({
+    mode: 'textareas',
+    theme: 'modern',
+    plugins: "autolink code colorpicker hr image link lists paste table textcolor",
+    menubar: '',
+    toolbar: "undo redo | styleselect | bold italic | link unlink image | alignleft aligncenter alignright | bullist numlist table | textcolor | code",
+    valid_elements : '*[*]',
+    elementpath: false,
+    statusbar: false
   });
 }
 
