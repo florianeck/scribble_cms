afb63f849034f1f713efc03a23afeb7b1c0050a6
Florian Eck <florian.eck@el-digital.de>
2017-06-21 13:52:09 +0200

    cleaner editor

--- app/assets/javascripts/scribbler.js
+++ app/assets/javascripts/scribbler.js
@@ -2,14 +2,17 @@
 // All this logic will automatically be available in application.js.
 
 window.initScribblerTinyMce = function(){
-  tinyMCE.init({
-    mode: 'textareas',
-    theme: 'modern',
-    plugins: "autolink code colorpicker hr image link lists paste table textcolor",
-    menubar: '',
-    toolbar: "undo redo | styleselect | bold italic | link unlink image | alignleft aligncenter alignright | bullist numlist table | textcolor | code",
-    valid_elements : '*[*]'
-   });
+  tinymce.remove("textarea")
+ tinyMCE.init({
+   mode: 'textareas',
+   theme: 'modern',
+   plugins: "autolink code colorpicker hr image link lists paste table textcolor",
+   menubar: '',
+   toolbar: "undo redo | styleselect | bold italic | link unlink image | alignleft aligncenter alignright | bullist numlist table | textcolor | code",
+   valid_elements : '*[*]',
+   elementpath: false,
+   statusbar: false
+  });
 }
 
 $(document).ready(function() {
