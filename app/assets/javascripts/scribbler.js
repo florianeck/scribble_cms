b4cea911fa7c6485baa16e87cf90a9cbffa68850
Florian Eck <florian.eck@el-digital.de>
2017-06-25 12:19:03 +0200

    update version - allow text size changes

--- app/assets/javascripts/scribbler.js
+++ app/assets/javascripts/scribbler.js
@@ -8,10 +8,11 @@ window.initScribblerTinyMce = function(){
     theme: 'modern',
     plugins: "autolink code colorpicker hr image link lists paste table textcolor",
     menubar: '',
-    toolbar: "undo redo | styleselect | bold italic | link unlink image | alignleft aligncenter alignright | bullist numlist table | textcolor | code",
+    toolbar: "undo redo | styleselect | bold italic | link unlink image | alignleft aligncenter alignright | bullist numlist table | textcolor fontsizeselect | code",
     valid_elements : '*[*]',
     elementpath: false,
-    statusbar: false
+    statusbar: true,
+    fontsize_formats: "8px 10px 12px 14px 16px 18px 24pt 36px 48px"
   });
 }
 
